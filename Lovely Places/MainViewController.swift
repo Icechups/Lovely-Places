98876b6146fb9428b32e98235c24fd154778a021
Ilya Perevozkin <Perevozkin01@icloud.com>
2021-03-20 18:23:20 +0500

    Initial Commit

---
+++ Lovely Places/MainViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MainViewController.swift
+//  Lovely Places
+//
+//  Created by Илья Перевозкин on 16.03.2021.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    let lakes = ["Иткуль","Банное","Сунгуль","Балтым","Старая линза","Шарташ","Верх-Исетский пруд"]
+    let seas = ["Чёрное море","Средиземное море","Красное море"]
+    let countries = ["Россия","Египет","Турция","Казахстан","Украина"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lakes.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = lakes[indexPath.row]
+        cell.imageView?.image = UIImage(named: lakes[indexPath.row])
+
+        return cell
+    }
+    
+
+   
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lovely Places/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Lovely Places
+//
+//  Created by Илья Перевозкин on 09.02.2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+    }
+    
+
+
+}
+
