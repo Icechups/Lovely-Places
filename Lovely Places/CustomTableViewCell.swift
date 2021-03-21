ca74d45d6c4d6c05ddf4bd3256320eddb8262699
Ilya Perevozkin <Perevozkin01@icloud.com>
2021-03-21 11:09:39 +0500

    Custom Table View Cell

---
+++ Lovely Places/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  Lovely Places
+//
+//  Created by Илья Перевозкин on 21.03.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
